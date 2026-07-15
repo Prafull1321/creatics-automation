dfb475622715177168a0033fde7921126ea16496
Prafull Talele <prafull.talale@cinequest.org>
2026-07-15 13:09:47 +0530

    Automation suite Execution

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("cypress");
 //const grep = require("cypress-grep/src/plugin");
-
+ 
 module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 120000,
   viewportWidth: 1280,
   viewportHeight: 720,
   retries: 2,
-
+ 
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     reportDir: "cypress/reports",
@@ -19,16 +19,16 @@ module.exports = defineConfig({
     embeddedScreenshots: true,
     inlineAssets: true,
   },
-
+ 
   projectId: "3uirpk",
-
+ 
   e2e: {
     baseUrl: "https://testing.creatics.org",
     chromeWebSecurity: false,
     experimentalModifyObstructiveThirdPartyCode: true,
     experimentalWebKitSupport: true,
     specPattern: "cypress/e2e/**/*.cy.js",
-
+ 
     setupNodeEvents(on, config) {
       //grep(config);
       require("cypress-mochawesome-reporter/plugin")(on);
@@ -37,13 +37,13 @@ module.exports = defineConfig({
   },
 });
 // const { defineConfig } = require("cypress");
-
-
+ 
+ 
 // module.exports = defineConfig({
 //   defaultCommandTimeout: 10000, // 10 seconds timeout for all commands
 //   pageLoadTimeout: 60000, // 60 seconds timeout for page load
 //   retries: 2, // retries attempts
-
+ 
 //   reporter: "cypress-mochawesome-reporter",
 //   reporterOptions: {
 //     reportDir: "cypress/reports", // Directory where reports are stored
@@ -55,27 +55,27 @@ module.exports = defineConfig({
 //     embeddedScreenshots: true, // Embed screenshots in the report
 //     inlineAssets: true, // Embed assets inline in the report
 //   },
-
+ 
 //    e2e: {
 //     setupNodeEvents(on, config) {
 //       grep(config);
 //       return config;
 //     }
 //   },
-
+ 
 //   e2e: {
 //     baseUrl: 'https://testing.creatics.org',
 //     chromeWebSecurity: false,
 //     experimentalModifyObstructiveThirdPartyCode: true
 //   },
-
+ 
 //   projectId: "3uirpk",   // <-- your actual projectId
 //   e2e: {
 //     setupNodeEvents(on, config) {
 //       // node events
 //     },
 //   },
-
+ 
 //   e2e: {
 //     setupNodeEvents(on, config) {
 //       // implement node event listeners here
